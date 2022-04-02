6028a424ec0c5a9dab896c4375c20f9340915fa2
Amr Abulseoud <amrabulseoud@gmail.com>
2022-04-02 05:02:35 +0200

    added random strings benchmark

--- bench.js
+++ bench.js
@@ -9,7 +9,11 @@ const suite = new Benchmark.Suite("hi-mom");
 
 
 const length = 10_000;
-const namesArr = new Array(10_000).fill(undefined).map(() => names[Math.floor(Math.random() * names.length)]);
+const namesArr = new Array(length).fill(undefined).map(() => names[Math.floor(Math.random() * names.length)]);
+
+const randomStrings = new Array(length).fill(undefined).map(() => {
+  return Math.floor(Math.random() * 100_000_000).toString(36);
+});
 
 let i = 0;
 
@@ -38,6 +42,13 @@ suite
   .add(`faster-hi-mom with ${length} names`, () => {
     fasterHiMom(namesArr[i++ % length]);
   }, opts)
+  .add(`hi-mom with ${length} random strings`, () => {
+    originalHiMom(randomStrings[i++ % length]);
+  }, opts)
+  .add(`faster-hi-mom with ${length} random strings`, () => {
+    fasterHiMom(randomStrings[i++ % length]);
+  }, opts)
+
   .on("cycle", function(event) {
     console.log(String(event.target));
   })
